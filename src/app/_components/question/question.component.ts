3caf36fa04481f9aee758dbd3bf242c66ebfd267
Driek Snellens <drieksnellens@Drieks-MacBook-Pro.local>
2023-11-22 20:35:55 +0100

    geformateerde code vragen en goede vraag feedback (zet geluid aan)

--- src/app/_components/question/question.component.ts
+++ src/app/_components/question/question.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Question } from '../../_models/question';
 import { Answer } from '../../_models/answer';
 import { GameService } from '../../_services/game.service';
 
+// Then register the languages you need
 @Component({
   selector: 'app-question',
   standalone: true,
@@ -11,12 +12,21 @@ import { GameService } from '../../_services/game.service';
   templateUrl: './question.component.html',
   styleUrl: './question.component.css'
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit {
   @Output() hideQuestionEvent = new EventEmitter();
   @Input() question: Question | null = null;
   selectedAnswer: Answer | null = null;
-
+  audio = new Audio();
   constructor(private gameService: GameService) {}
+
+  ngOnInit(): void {
+    this.audio = new Audio();
+    this.audio.src = "../../assets/ha-ha-ha-sukkel.mp3";
+    this.audio.load();
+    this.question?.options.forEach(options => {
+      options.answer = decodeURIComponent(options.answer);
+    })
+  }
 
   selectAnswer(answer: Answer) {
     this.selectedAnswer = answer;
@@ -27,7 +37,7 @@ export class QuestionComponent {
       this.gameService.nextScene();
       this.hideQuestionEvent.emit();
     } else {
-      console.log('Sukkel');
+      this.audio.play();
     }
   }
 }
